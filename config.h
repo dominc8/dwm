bbdf43722946273f8e76db60ae0440d639811f64
dominc8 <kormandominik@gmail.com>
2020-10-31 10:33:59 +0100

    Add rotatestack patch

--- config.h
+++ config.h
@@ -79,6 +79,8 @@ static Key keys[] = {
     { MODKEY,               XK_p,               spawn,          { .v = dmenucmd } },
     { MODKEY|ShiftMask,     XK_Return,          spawn,          { .v = termcmd  } },
     { MODKEY,               XK_b,               togglebar,      { 0 } },
+    { MODKEY|ShiftMask,     XK_n,               rotatestack,    {.i = +1 } },
+    { MODKEY|ShiftMask,     XK_m,               rotatestack,    {.i = -1 } },
     { MODKEY,               XK_j,               focusstack,     { .i = +1 } },
     { MODKEY,               XK_k,               focusstack,     { .i = -1 } },
     { MODKEY,               XK_i,               incnmaster,     { .i = +1 } },
