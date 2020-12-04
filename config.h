a7118d780345d26a492767a40500bcca9c0d5cb2
dominc8 <kormandominik@gmail.com>
2020-12-04 21:46:42 +0100

    Set xpad to be floating on default

--- config.h
+++ config.h
@@ -39,6 +39,7 @@ static const Rule rules[] = {
     { "st-256color",    NULL,       NULL,           0,          0,          -1 },
     { NULL,             NULL,       "Event Tester", 0,          0,          -1 }, /* xev */
     { "safeeyes",       NULL,       NULL,           0,          1,          -1 },
+    { "xpad",           NULL,       NULL,           0,          1,          -1 },
 };
 
 /* layout(s) */
